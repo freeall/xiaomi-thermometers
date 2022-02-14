077874ad83e6ee1cf88ee216aa0315380f80476f
Tobias Baunbaek <freeall@gmail.com>
2022-02-14 15:50:17 +0000

    Add parser

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const axios = require('axios')
+const { Parser } = require('binary-parser')
 
 const app = express()
+const xiaomiMijiaParser = new Parser()
+  .endianess('little')
+  .buffer('mac', { length: 6 })
+  .int16('temperature')
+  .int16('humidity')
+  .int16('batteryVoltage')
+  .uint8('batteryLevel')
+  .uint8('counter')
+  .uint8('flags')
 
 app.post('/', express.json(), (req, res) => {
-  console.log('body', req.body)
+  const { data } = req.body
+  const buff = Buffer.from(data, 'hex')
+  const parsed = xiaomiMijiaParser.parse(buff)
+  parsed.temperature = parsed.temperature / 100
+  parsed.humidity = parsed.humidity / 100
+  parsed.batteryVoltage = parsed.batteryVoltage / 1000
+  console.log('Received', parsed)
   res.send('ok')
 })
 
